3c2126bb0f7ed685eccba5e62631406b1faa0fd9
natalia <natikrein@gmail.com>
2021-09-25 16:10:57 -0300

    ajustando

--- src/components/SearchBooks/index.js
+++ src/components/SearchBooks/index.js
@@ -32,7 +32,6 @@ export const SearchBooks = () => {
                 })) 
                 setIsLoading(false)
                 setsearchingBooks(searchingBooks);
-                console.log(searchingBooks)
         })
       };
 
